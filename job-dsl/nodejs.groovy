4b98d2fd410a688ef697e714c080ef8b1d664077
EC2 Default User <ec2-user@ip-172-31-21-240.ec2.internal>
2019-12-09 21:40:55 +0000

    changing repo for job dsl

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/tachomi/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('tachomi@dummy.com')
         }
     }
     triggers {
